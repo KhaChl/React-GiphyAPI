7faacbd9d7c88a8cb5f90660006ac6eeffeb8a31
Kevin <kpizarro.m@outlook.com>
2020-07-15 19:46:13 -0400

    Docs

---
+++ docs/precache-manifest.cd223f9238f8beaec7303fa28e0fa310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ed127b63120125896d9540bef7da20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6694cb66256c47b92c0",
+    "url": "/static/css/main.737e9b75.chunk.css"
+  },
+  {
+    "revision": "71522f13fd6174d0aef1",
+    "url": "/static/js/2.c90e9a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c90e9a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6694cb66256c47b92c0",
+    "url": "/static/js/main.9fba785e.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
